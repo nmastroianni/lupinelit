1dfda561fad1cd83445417fe082d3a703ca58be8
Neil Mastroianni <suancho@gmail.com>
2022-08-11 22:49:32 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,27 @@
 module.exports = {
   siteMetadata: {
     title: `lupinelit`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.lupineliterature.com`,
   },
-  plugins: ["gatsby-plugin-postcss", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
     },
-    __key: "images"
-  }]
-};
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+}
